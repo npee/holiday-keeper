45ff5854d51e27294dd07b94cded531400e8e0b8
npee <jmc4664@gmail.com>
2025-12-03 13:41:02 +0900

    Initial Commit

---
+++ src/test/java/io/npee/holidaykeeper/HolidayKeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package io.npee.holidaykeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolidayKeeperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
