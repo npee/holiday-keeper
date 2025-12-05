272fa71ae134bbf31ce21bdeb4d8978656f4950b
npee <jmc4664@gmail.com>
2025-12-05 18:52:25 +0900

    5년치 휴일 데이터 리로드 서비스 작성

---
+++ src/main/java/io/npee/holidaykeeper/domain/service/HolidayReloadService.java
@@ -0,0 +1,127 @@
+package io.npee.holidaykeeper.domain.service;
+
+import io.npee.holidaykeeper.domain.model.holiday.ExternalHoliday;
+import io.npee.holidaykeeper.domain.model.holiday.Holiday;
+import io.npee.holidaykeeper.domain.model.holiday.HolidayRegion;
+import io.npee.holidaykeeper.domain.model.region.Region;
+import io.npee.holidaykeeper.domain.repository.HolidayRegionRepository;
+import io.npee.holidaykeeper.domain.repository.HolidayRepository;
+import io.npee.holidaykeeper.domain.repository.RegionRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class HolidayReloadService {
+
+    private final HolidayExternalFetchService holidayExternalFetchService;
+
+    private final HolidayRepository holidayRepository;
+    private final HolidayRegionRepository holidayRegionRepository;
+    private final RegionRepository regionRepository;
+
+    @Transactional
+    public void reloadHolidays() {
+        long startTime = System.currentTimeMillis();
+        log.info("휴일 데이터 리로드 시작");
+        holidayRegionRepository.deleteAllInBatch();
+        holidayRepository.deleteAllInBatch();
+
+        log.info("최근 5년간의 휴일 데이터를 가져오는 중...");
+        List<ExternalHoliday> fetchHolidays = holidayExternalFetchService.fetchAllHolidaysForRecentFiveYears();
+        log.info("휴일 데이터 {}건 조회 완료", fetchHolidays.size());
+
+        Set<String> isoCodes = new HashSet<>();
+
+        for (ExternalHoliday ext : fetchHolidays) {
+            isoCodes.add(ext.getCountryCode());
+
+            if (ext.getCounties() != null) {
+                isoCodes.addAll(ext.getCounties());
+            }
+        }
+
+        Map<String, Region> existing = regionRepository.findAll().stream()
+                .collect(Collectors.toMap(Region::getIsoCode, r -> r));
+
+        List<Region> newRegions = isoCodes.stream()
+                .filter(iso -> !existing.containsKey(iso))
+                .map(iso -> {
+                    String[] parts = iso.split("-");
+                    String countryCode = parts[0];
+                    String regionCode = parts.length > 1 ? parts[1] : null;
+                    return Region.builder()
+                            .countryCode(countryCode)
+                            .regionCode(regionCode)
+                            .isoCode(iso)
+                            .build();
+                })
+                .toList();
+
+        regionRepository.saveAll(newRegions);
+
+        Map<String, Region> regionByIso = regionRepository.findAll().stream()
+                .collect(Collectors.toMap(Region::getIsoCode, r -> r));
+
+        List<Holiday> holidays = new ArrayList<>();
+
+        for (ExternalHoliday extHoliday : fetchHolidays) {
+            Holiday holiday = Holiday.builder()
+                    .date(LocalDate.parse(extHoliday.getDate()))
+                    .localName(extHoliday.getLocalName())
+                    .name(extHoliday.getName())
+                    .countryCode(extHoliday.getCountryCode())
+                    .fixed(extHoliday.isFixed())
+                    .global(extHoliday.isGlobal())
+                    .launchYear(extHoliday.getLaunchYear())
+                    .build();
+
+            if (extHoliday.getTypes() != null && !extHoliday.getTypes().isEmpty()) {
+                holiday.getTypes().addAll(extHoliday.getTypes());
+            }
+
+            List<String> extCounties = extHoliday.getCounties();
+
+            if (extCounties == null || extCounties.isEmpty()) {
+                String isoCode = extHoliday.getCountryCode();
+                Region region = regionByIso.get(isoCode);
+                if (region != null) {
+                    holiday.getHolidayRegions().add(
+                            HolidayRegion.builder()
+                                    .holiday(holiday)
+                                    .region(region)
+                                    .build()
+                    );
+                }
+            } else {
+                for (String isoCode : extCounties) {
+                    Region region = regionByIso.get(isoCode);
+                    if (region != null) {
+                        holiday.getHolidayRegions().add(
+                                HolidayRegion.builder()
+                                        .holiday(holiday)
+                                        .region(region)
+                                        .build()
+                        );
+                    } else {
+                        log.warn("isoCode {}에 해당하는 Region이 존재하지 않습니다.", isoCode);
+                    }
+                }
+            }
+            holidays.add(holiday);
+        }
+
+        List<Holiday> savedHolidays = holidayRepository.saveAll(holidays);
+        log.info("휴일 데이터 {}건 저장 완료", savedHolidays.size());
+
+        long endTime = System.currentTimeMillis();
+        log.info("휴일 데이터 리로드 완료. 소요 시간: {} ms", (endTime - startTime));
+    }
+}
